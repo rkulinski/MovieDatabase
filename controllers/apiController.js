dd18dc089b504036c7345296482286466866d375
DESKTOP-TI0JA8P\Robert <robert.kulinski93@gmail.com>
2017-09-29 20:12:29 +0200

    Refactor

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,26 +1,23 @@
-var Movies = require('../models/movieModel');
-var Comments = require('../models/commentModel');
-var bodyParser = require('body-parser');
-var fetch = require('node-fetch');
+var Movies = require('../models/movieModel'),
+    Comments = require('../models/commentModel'),
+    bodyParser = require('body-parser'),
+    fetch = require('node-fetch');
 
-module.exports = function(app) {
+module.exports = app => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.post('/api/movies', function(req, res) {
-
+    app.post('/api/movies', (req, res)=> {
         let movie_title = req.body.title;
 
         fetch(`http://www.omdbapi.com/?apikey=PlsBanMe&t=${movie_title}`)
-        .then(function(res) {
-            return res.json();
-        }).then(function(movie_obj) {
-            console.log(movie_obj);
-            if(movie_obj.Title) {
+        .then(res => res.json())
+        .then(movie_obj => {
+            if(!!movie_obj.Title) {
                 let newMovie = Movies(movie_obj);
                 
-                newMovie.save(function(err) {
+                newMovie.save(err => {
                     if (err) throw err;
                     res.send(newMovie);
                 });   
@@ -31,26 +28,23 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/movies', function(req, res) {
-
-        Movies.find({}, 'Title', function(err, movies) {
+    app.get('/api/movies', (req, res) => {
+        Movies.find({}, 'Title', (err, movies) => {
             if(err) throw err;
             res.send(movies);
         });
-
     });
 
-    app.post('/api/comments', function(req, res) {
-
-        Movies.findById(req.body.movie_id, function (err, found) {
-            if(found) {
+    app.post('/api/comments', (req, res) => {
+        Movies.findById(req.body.movie_id, (err, found) => {
+            if(!!found) {
                 if(req.body.movie_id) {
                     let newComment = Comments({
                         MovieID: req.body.movie_id,
                         Comment: req.body.comment     
                     });
                     
-                    newComment.save(function(err) {
+                    newComment.save(err => {
                         if(err) throw err;
                         res.send(newComment);
                     });   
@@ -63,26 +57,20 @@ module.exports = function(app) {
                 res.send("Error. There's no movie with that id");
             }  
         });
-
     });
 
-    app.get('/api/comments', function(req, res) {
-        console.log(req.query.movie_id);
-        if(req.query.movie_id) {
-            
-            Comments.find({ MovieID: req.query.movie_id }, function(err, comments) {
+    app.get('/api/comments', (req, res) => {
+        if(!!req.query.movie_id) {
+            Comments.find({ MovieID: req.query.movie_id }, (err, comments) => {
                 if(err) throw err;
                 res.send(comments);
             });
-
         }
         else {
-
-            Comments.find(function(err, comments) {
+            Comments.find((err, comments) => {
                 if(err) throw err;
                 res.send(comments);
             });
-    
         }
     });
 }
